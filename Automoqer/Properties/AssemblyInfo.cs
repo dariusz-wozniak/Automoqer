530bb648caa7fbc49432c6f07d064e958472fb52
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2021-07-03 21:23:04 +0200

    chore: update description and project URL + bump version

--- Automoqer/Properties/AssemblyInfo.cs
+++ Automoqer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("dariusz-wozniak-Automoqer")]
-[assembly: AssemblyDescription("Automatically creates services for unit testing with constructor parameters as Moq-objects. Docs and info are available on the project's github site https://github.com/rbengtsson/Automoqer")]
+[assembly: AssemblyDescription("Fork of AutoMoqer (with updated packages)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robert Bengtsson")]
 [assembly: AssemblyProduct("dariusz-wozniak-Automoqer")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
